91765771ee627d981bafac557d0cde4626bd3907
Akos Menyhert <menyhert.akos@gmail.com>
2014-12-02 16:41:41 +0100

    Removed unused variables

--- prototypes/org.eclipse.incquery.examples.cps.m2t.proto.distributed/src/org/eclipse/incquery/examples/cps/m2t/proto/distributed/hosts/applications/BaseApplication.java
+++ prototypes/org.eclipse.incquery.examples.cps.m2t.proto.distributed/src/org/eclipse/incquery/examples/cps/m2t/proto/distributed/hosts/applications/BaseApplication.java
@@ -1,24 +1,12 @@
 package org.eclipse.incquery.examples.cps.m2t.proto.distributed.hosts.applications;
 
-import java.util.Date;
-import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.eclipse.incquery.examples.cps.m2t.proto.distributed.hosts.Host;
 import org.eclipse.incquery.examples.cps.m2t.proto.distributed.hosts.statemachines.State;
 
-import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.Table;
-
 public class BaseApplication<StateType extends State> implements Application {
 
 	private static Logger logger = Logger.getLogger("cps.proto.distributed.application");
-	
-	// Table<AppID, TriggerID, Date>
-	Table<String, String, Date> triggers = HashBasedTable.create();
-	
-	// Applications
-	List<Application> applications;
 	
 	protected StateType currentState;
 	protected Host host;
