9e7564ed51d2a7887f174828a0654b697c0129d3
Abel Hegedus <abel.hegedus@incquerylabs.com>
2016-05-09 13:17:30 +0200

    remove performance test related content

--- scripts/report.bat
+++
@@ -1,1 +0,0 @@
-python ../../mondo-sam/reporting/report.py --source ../../incquery-examples-cps/tests/org.eclipse.incquery.examples.cps.performance.tests/results/results.csv --output ../../incquery-examples-cps/diagrams/ --config ../../incquery-examples-cps/scripts/config.json
